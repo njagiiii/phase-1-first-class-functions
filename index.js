97b1de998e0cc9afcd90013d3cabcedc3e5d5e92
njagiiii <grace.makena@student.moringaschool.com>
2023-06-15 21:02:42 -0400

    completed assignment

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const chai = require("chai");
 const spies = require("chai-spies");
 chai.use(spies);
 
-function receivesAFunction(callback){
-    return (callback());
+function receivesAFunction(name){
+    return (name());
  }
 
 
